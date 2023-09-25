99b24dd13d76854e0ec90aaec4ee9af12dd5ab39
Arnaud F. <25860666+arnaudfl@users.noreply.github.com>
2023-09-25 17:38:00 +0200

    style: :art: change category title style

--- src/routes/categories/[slug]/+page.svelte
+++ src/routes/categories/[slug]/+page.svelte
@@ -12,11 +12,14 @@
 
 <!-- Posts -->
 <section>
-    <h2>Catégorie : {data.category}</h2>
+    <h2 class="tags">
+        <span class="surface-4 category">Catégorie :</span>
+		<a href="/categories/{data.category}" class="surface-4">&num;{data.category}</a>
+	</h2>
 	<ul class="posts">
 		{#each data.postsFiltered as post}
 			<li class="post">
-				<a href=../{post.slug} class="title">{post.title}</a>
+				<a href=/{post.slug} class="title">{post.title}</a>
 				<p class="date">{formatDate(post.date)}</p>
 				<p class="description">{post.description}</p>
 			</li>
