321e6f0d504bcd6b2530f597b2aa643ef1e1db10
Arnaud F. <25860666+arnaudfl@users.noreply.github.com>
2023-09-26 13:50:55 +0200

    fix: fix css

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -16,10 +16,6 @@
 		padding-block: var(--size-7);
 	}
 
-	.links {
-		margin-block: var(--size-7);
-	}
-
 	a {
 		color: inherit;
 		text-decoration: none;
@@ -29,12 +25,6 @@
 		nav {
 			display: flex;
 			justify-content: space-between;
-		}
-
-		.links {
-			display: flex;
-			gap: var(--size-7);
-			margin-block: 0;
 		}
 	}
 </style>
